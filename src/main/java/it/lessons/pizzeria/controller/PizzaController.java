9430033d03c35a80e7b2cfa56302eca1ed5f3b4f
Rajbir-Singh-Create <rajbir.singh.create@gmail.com>
2025-04-08 14:08:50 +0200

    feat: Repository and DB added

--- src/main/java/it/lessons/pizzeria/controller/PizzaController.java
+++ src/main/java/it/lessons/pizzeria/controller/PizzaController.java
@@ -1,27 +1,31 @@
 package it.lessons.pizzeria.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import it.lessons.pizzeria.model.Pizza;
+import it.lessons.pizzeria.repository.PizzaRepository;
 
 
 @Controller
-@RequestMapping("/pizza")
+@RequestMapping("/pizzas")
 public class PizzaController {
 
-    @GetMapping("path")
-    public String index(@RequestParam String param, Model model) {
-        List<Pizza> result = new ArrayList<>();
+    // Inietto la repository per utilizzarne i metodi concretizzati dalla IoC
+    @Autowired 
+    private PizzaRepository pizzaRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        List<Pizza> result = pizzaRepository.findAll();
         model.addAttribute("list", result);
         
-        return "/pizza/index";
+        return "/pizzas/index";
     }
     
 }
