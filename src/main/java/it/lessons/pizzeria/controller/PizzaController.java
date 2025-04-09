fe855a010c19e5a10a0b08ba5c955574a9a39e35
Rajbir-Singh-Create <rajbir.singh.create@gmail.com>
2025-04-09 21:01:33 +0200

    feat: validation added

--- src/main/java/it/lessons/pizzeria/controller/PizzaController.java
+++ src/main/java/it/lessons/pizzeria/controller/PizzaController.java
@@ -6,15 +6,18 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import it.lessons.pizzeria.model.Pizza;
 import it.lessons.pizzeria.repository.PizzaRepository;
+import jakarta.validation.Valid;
 
 
 
@@ -58,17 +61,23 @@ public class PizzaController {
     @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("pizza", new Pizza());
-
         return "/pizzas/create";
     }
     
     // Metodo POST per la form
     @PostMapping("/create")
-    public String store(@ModelAttribute("pizza") Pizza formPizza, Model model) {
+    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
         // Logica di validazione
+        if(bindingResult.hasErrors()){
+            // Se il BindingResult ha dato errori
+            // ritorno alla pagina create = resto nella pagina
+            return "/pizzas/create";
+        }
 
         // Logica di salvataggio
         pizzaRepository.save(formPizza);
+
+        redirectAttributes.addFlashAttribute("successMessage", "Pizza created successfully!");
         return "redirect:/pizzas";
     }
     
